bb7b939b059fed483073b5e4a349dc8d08a7181d
Sven-Hendrik Haase <svenstaro@gmail.com>
2018-08-16 05:03:09 +0200

    Disable network device loopback test as it doesn't work in Docker

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 phoronix-test-suite system-info
 phoronix-test-suite system-sensors
 phoronix-test-suite network-info
-yes | phoronix-test-suite default-run build-linux-kernel compress-7zip ffmpeg nginx redis sqlite stream network-loopback
+yes | phoronix-test-suite default-run build-linux-kernel compress-7zip ffmpeg nginx redis sqlite stream
